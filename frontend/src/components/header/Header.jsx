a0945e576a32002fbeab72e31fc6103044a474ae
Tiago Fernandes Avila <tiagofernandesavila100@gmail.com>
2025-08-26 19:40:26 -0300

    header componenta

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -3,30 +3,17 @@ import { LinkedIcon, GithubIcon } from "../../assets/Icons.jsx"
 
 function Header() {
     return (
-        <div>
-            <div>
-                <header className="header">
-                    <h1>Blog Do Tiago</h1>
-                    <nav>
-                        <ul>
-                            <li>Home</li>
-                            <li><a href="#">icone linkedin <LinkedIcon size={32} color="#404040 "/></a></li>
-                            <li><a href="#">icone github/portfolio <GithubIcon size={32} color="#404040"/></a></li>
-                        </ul>
-                        <ul>
-                            
-
-                            <a href="#"> login</a>
-                            <a href="#">cadastre-se</a>
-                        </ul>
-                    </nav>
-                </header>
-            </div>
-
-
-
-
-        </div>
+        <header className="navbar">
+  <h1 className="logo">Blog Do Tiago</h1>
+  
+  <nav className="nav-links">
+    <a href="#" className="home">home</a>
+    <a href="#"><LinkedIcon size={24} color="#333" className="icon" /></a>
+    <a href="#"><GithubIcon size={24} color="#333" className="icon" /></a>
+    <a href="#">Login</a>
+    <a href="#">Cadastro</a>
+  </nav>
+</header>
     )
 }
 
